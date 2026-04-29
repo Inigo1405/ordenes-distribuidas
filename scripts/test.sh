6e52b1105902d14f131c31011ff2e5913c9c9de4
inigo1405 <quintanadinigo@gmail.com>
2026-04-29 09:14:11 -0600

    Enhance test script: add additional curl commands for orders, signup, and bookings

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,19 @@ set -e
 
 echo "Ejecutando pruebas..."
 
+curl -X POST http://localhost:8000/orders \
+  -H "Content-Type: application/json" \
+  -d '{"customer": "Berny", "items": [{"sku": "A1", "qty": 2}]}'
+
+
+curl -X POST http://localhost:8000/auth/signup \
+  -H "Content-Type: application/json" \
+  -d '{"email": "inigo@iberopuebla.mx","password": "123", "name": "Inigo"}'
+
+
+curl -X POST http://localhost:8000/bookings \
+  -H "Content-Type: application/json" \
+  -d '{"guest": "Pingul Barranco","room_type": "double","check_in": "2026-05-01","check_out": "2026-05-05"}'
 
 
 echo "Pruebas terminadas exitosamente! (‾◡◝)"
